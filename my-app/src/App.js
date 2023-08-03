1e925d2e2dfaedd0f22bd527e86a02ea08e5bdca
MaratRizatdinov <maratrizatdinov91@gmail.com>
2023-08-03 12:43:50 +0300

    hw1

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">      
+      Здесь скоро будет Skypro.Music
+    </div>
+  );
+}
+
+export default App;
